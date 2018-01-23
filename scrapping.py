be369803e906c45c59c09e3b8cf85b326529da98
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-01-22 22:48:04 -0300

    update get_url

--- scrapping.py
+++ scrapping.py
@@ -1,22 +1,17 @@
 import requests, bs4, lxml, json
 
 
-def get_location():
+def get_url():
     site = "http://freegeoip.net/json/"
-
     req = requests.get(site)
-
     retorno = json.loads(req.text)
-
-    codigo_cidade = retorno["region_code"].lower()
-
-    codigo_cidade = codigo_cidade + "eae men"
-    print(codigo_cidade)   
-
+    codigo_cidade = retorno["region_code"].lower()  
+    codigo_olx = "http://" + codigo_cidade + ".olx.com.br"
+    return codigo_olx
 
 
 
 if __name__ == "__main__":
-    get_location()
+    get_url()
 
 # {"key": value, key2:felix, key3:tarcisio}
